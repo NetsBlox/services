709ee05d08f5ad5134499485e80ff2e45a702e5d
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2022-12-15 15:43:36 -0600

    Copy old version from old repo

--- test/procedures/iotscape.spec.js
+++ test/procedures/iotscape.spec.js
@@ -1,10 +1,11 @@
-const utils = require('../assets/utils');
+const utils = require('../../../../assets/utils');
 
 describe(utils.suiteName(__filename), function() {
 
     utils.verifyRPCInterfaces('IoTScape', [
         ['getDevices', ['service']],
         ['getMessageTypes', ['service']],
+        ['getMethods', ['service']],
         ['getServices', []],
         ['send', ['service', 'id', 'command']],
     ]);
