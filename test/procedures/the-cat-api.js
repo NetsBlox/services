eac618328d5882345c1bd3106751ddfc68b0aace
Oluwatito Ebiwonjumi <31973148+ebiwonjumit@users.noreply.github.com>
2023-02-03 10:08:50 -0600

    Updated tests

--- test/procedures/the-cat-api.js
+++ test/procedures/the-cat-api.js
@@ -1,16 +1,9 @@
 const utils = require("../assets/utils");
 
-<<<<<<< HEAD
 describe(utils.suiteName(__filename), function() {
     utils.verifyRPCInterfaces('TheCatApi', [
         ['getRandomCatImage', ['catBreed']],
         ['getCatBreeds', []],
         ['getBreedInfo', ['catBreed']],
     ]);
-=======
-describe(utils.suiteName(__filename), function () {
-  utils.verifyRPCInterfaces("TheCatApi", [
-    ["getRandomCatImage", ["catBreeds"]],
-  ]);
->>>>>>> 765e96475f26c1bf2e25c8e22c160a9132a6104f
 });
