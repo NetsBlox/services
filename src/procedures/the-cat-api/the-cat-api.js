0f4c016fd6237144082fca9e52d2b3f5e8b75b80
Oluwatito Ebiwonjumi <31973148+ebiwonjumit@users.noreply.github.com>
2023-02-06 13:52:05 -0600

    Dog&Cat Services (#96)
    * Dog&Cat Services
    
    * Fix code formatting
    
    * Cat-API issue resolved
    
    * Addressed Devin's requested changes
    
    * Missed a comma
    
    * Fix code formatting
    
    * Fixed Typo
    
    * Updated jsdoc
    
    * fixed JsDoc syntax error
    
    ---------
    
    Co-authored-by: Format Bot <format-bot@netsblox.org>

---
+++ src/procedures/the-cat-api/the-cat-api.js
@@ -0,0 +1,51 @@
+/**
+ * The Cats Service provides access to the Cat API data from https://api.thecatapi.com/v1/.
+ *
+ * @alpha
+ * @service
+ * @category Society
+ * @category Media
+ */
+
+const { TheCatApiKey } = require("../utils/api-key");
+const { registerTypes, CAT_BREEDS } = require("./types");
+const ApiConsumer = require("../utils/api-consumer");
+registerTypes();
+
+// Cats API Url
+const catApiUrl = "https://api.thecatapi.com/v1/images/search";
+
+const TheCatApi = new ApiConsumer("TheCatApi", catApiUrl, {
+  cache: { ttl: 1 },
+});
+ApiConsumer.setRequiredApiKey(TheCatApi, TheCatApiKey);
+
+/**
+ * Get random cat image.
+ * @param {CatBreed=} catBreed provides a specific breed to search for. If omitted, no-breed-based filtering is performed.
+ * @returns {Image} the requested image
+ */
+TheCatApi.getRandomCatImage = async function (catBreed = "") {
+  //Requesting JSON from the Cat Api Url
+  const catJson = await this._requestData({
+    baseUrl: "https://api.thecatapi.com/v1/images/search?t=" + Date.now(),
+    queryString: "&breed_ids=" + catBreed,
+  });
+
+  //Get the image URL from the received JSON
+  const imageUrl = catJson[0].url;
+
+  return this._sendImage({
+    baseUrl: imageUrl,
+  });
+};
+
+/**
+ * Get list of cat breeds.
+ * @returns {CatBreed[]} list of cat breeds supported by API.
+ */
+TheCatApi.getCatBreeds = function () {
+  return Object.keys(CAT_BREEDS);
+};
+
+module.exports = TheCatApi;
---
+++ src/procedures/the-dog-api/the-dog-api.js
@@ -0,0 +1,51 @@
+/**
+ * The Dogs Service provides access to the Dog API data from https://api.thedogapi.com/v1/.
+ *
+ * @alpha
+ * @service
+ * @category Society
+ * @category Media
+ */
+
+const { TheDogApiKey } = require("../utils/api-key");
+const { registerTypes, DOG_BREEDS } = require("./types");
+const ApiConsumer = require("../utils/api-consumer");
+registerTypes();
+
+// Dogs API Url
+const dogApiUrl = "https://api.thedogapi.com/v1/images/search";
+
+const TheDogApi = new ApiConsumer("TheDogApi", dogApiUrl, {
+  cache: { ttl: 1 },
+});
+ApiConsumer.setRequiredApiKey(TheDogApi, TheDogApiKey);
+
+/**
+ * Get random dog image.
+ * @param {DogBreed=} dogBreed provides a specific breed to search for. If omitted, no-breed-based filtering is performed.
+ * @returns {Image} the requested image
+ */
+TheDogApi.getRandomDogImage = async function (dogBreed = "") {
+  //Requesting JSON from the Dog Api Url
+  const dogJson = await this._requestData({
+    baseUrl: "https://api.thedogapi.com/v1/images/search?t=" + Date.now(),
+    queryString: "&breed_ids=" + dogBreed,
+  });
+
+  //Get the image URL from the received JSON
+  const imageUrl = dogJson[0].url;
+
+  return this._sendImage({
+    baseUrl: imageUrl,
+  });
+};
+
+/**
+ * Get list of dog breeds.
+ * @returns {DogBreed[]} list of dog breeds supported by API.
+ */
+TheDogApi.getDogBreeds = function () {
+  return Object.keys(DOG_BREEDS);
+};
+
+module.exports = TheDogApi;
