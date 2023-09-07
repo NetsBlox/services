94f71faf67e14ba3f18ce3c1ab91212fcf7ca591
Devin Jean <emailcruzjean@yahoo.com>
2023-09-07 09:17:51 -0500

    add YearSince type (#144)
    * add YearSince type
    
    * Fix code formatting
    
    * add tests
    
    * Fix code formatting
    
    ---------
    
    Co-authored-by: Format Bot <format-bot@netsblox.org>
    Co-authored-by: Brian Broll <brian.broll@gmail.com>

--- src/input-types.js
+++ src/input-types.js
@@ -532,6 +532,14 @@ defineType({
 });
 
 defineType({
+  name: "YearSince",
+  description:
+    "A year starting at some point and ranging up to the current year",
+  baseType: "BoundedInteger",
+  baseParams: (p) => [p[0], new Date().getFullYear()],
+});
+
+defineType({
   name: "BoundedString",
   description: "A string (text) with a minimum and/or maximum length.",
   baseType: "String",
