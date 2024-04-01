b1056009756ea06013de145f264cff32c4f2cfe7
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-04-01 16:23:22 -0500

    HTTP announce endpoint

--- src/procedures/iotscape/routes.js
+++ src/procedures/iotscape/routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express();
+const logger = require("../utils/logger")("iotscape-routes");
+const IoTScape = require("./iotscape");
+const bodyParser = require("body-parser");
 
 router.get(
   "/port",
@@ -9,5 +12,15 @@ router.get(
     );
   },
 );
+
+router.post(
+  "/announce",
+  bodyParser.json({ limit: "1mb" }),
+  async (req, res) => {
+    logger.info(`HTTP announcement from ${req.ip}`);
+    IoTScape._createService(req.body);
+    res.status(200).send("OK");
+  },
+)
 
 module.exports = router;
