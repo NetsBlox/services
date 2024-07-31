07c2cf475753508aa1e3951c30dea039d1a399ff
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-07-30 19:08:01 -0500

    Route for HTTP responses to IoTScape messages

--- src/procedures/iotscape/routes.js
+++ src/procedures/iotscape/routes.js
@@ -3,6 +3,8 @@ const router = express();
 const logger = require("../utils/logger")("iotscape-routes");
 const IoTScape = require("./iotscape");
 const bodyParser = require("body-parser");
+const IoTScapeDevices = require("./iotscape-devices");
+const IoTScapeServices = require("./iotscape-services");
 
 router.get(
   "/port",
@@ -22,5 +24,27 @@ router.post(
     res.status(200).send("OK");
   },
 );
+
+router.post(
+  "/response",
+  bodyParser.json({ limit: "5mb" }),
+  async (req, res) => {
+    if (req.body) {
+      // Validate fields
+      if (!req.body.request || !req.body.response || !Array.isArray(req.body.response)) {
+        return res.status(400).send("Invalid request: missing fields");
+      }
+      
+      if(Object.keys(IoTScapeServices._awaitingRequests).includes(req.body.request)) {
+        IoTScapeServices._awaitingRequests[req.body.request].resolve(...req.body.response);
+        return res.status(200).send("OK");
+      } else {
+        return res.status(400).send("No request found for this response.");
+      }
+    }
+
+    res.status(400).send("Invalid request.");
+  }
+)
 
 module.exports = router;
