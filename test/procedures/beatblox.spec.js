aebece77a12f16f1dccce35a7db26d6faf8483e4
Oluwatito Ebiwonjumi <31973148+ebiwonjumit@users.noreply.github.com>
2024-08-07 12:59:54 -0500

    Updated tests.

--- test/procedures/beatblox.spec.js
+++ test/procedures/beatblox.spec.js
@@ -2,13 +2,13 @@ const utils = require("../assets/utils");
 
 describe(utils.suiteName(__filename), function () {
   const MockService = require("../assets/mock-service");
-  const MusicApp = utils.reqSrc(
+  const BeatBlox = utils.reqSrc(
     "procedures/music-app/music-app",
   );
   const assert = require("assert");
   let service, testSuite;
 
-  utils.verifyRPCInterfaces("MusicApp", [
+  utils.verifyRPCInterfaces("BeatBlox", [
     ["getSoundNames", ["chords", "key", "bpm", "instrumentName"]],
     ["nameToSound", ["nameOfSound"]],
     ["getDrumOneShotNames", ["packName", "drumType"]],
@@ -18,7 +18,7 @@ describe(utils.suiteName(__filename), function () {
 
   before(async () => {
     testSuite = await utils.TestSuiteBuilder().setup();
-    service = new MockService(MusicApp);
+    service = new MockService(BeatBlox);
   });
   after(() => {
     testSuite.takedown();
