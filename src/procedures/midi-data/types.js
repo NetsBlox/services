23369948ff4289bdee10f11a3cf95f09d82e484c
Gabe Barnard <gbarnard08@gmail.com>
2024-12-09 13:38:31 -0600

    added midi-data rpc

---
+++ src/procedures/midi-data/types.js
@@ -0,0 +1,48 @@
+const types = require("../../input-types");
+
+const COMPOSERS = [
+  "J.S. Bach",
+  "Beethoven",
+  "Mozart",
+  "Haydn",
+  "Debussy",
+  "Traditional",
+  "O. Comeau",
+  "S. C. Foster",
+  "C. J. Brown",
+  "F. Abt",
+  "E. Grieg",
+  "E. Satie",
+  "Spagnoletti",
+  "Kruetzer",
+  "Yaniewicz",
+];
+
+const STYLES = [
+  "Classical",
+  "Baroque",
+  "Folk",
+  "Gospel",
+  "Hymn",
+  "Modern",
+  "Romantic",
+  "Popular / Dance",
+];
+
+function registerTypes() {
+  types.defineType({
+    name: "Composers",
+    description: "List of available composers",
+    baseType: "Enum",
+    baseParams: COMPOSERS,
+  });
+  
+  types.defineType({
+    name: "Styles",
+    description: "List of available styles",
+    baseType: "Enum",
+    baseParams: STYLES,
+  });
+}
+
+module.exports = { registerTypes };
